c887bfaab914e9d35e18a0b8c5499dbf51337e9d
peroperje <el.pionir@gmail.com>
2017-06-27 23:10:32 +0200

    delete build

--- src/components/static/Home.js
+++ src/components/static/Home.js
@@ -4,37 +4,18 @@
 
 import React from 'react';
 
-import Slider from '../slider/Slider';
-import SliderItem from '../slider/SliderItem';
 
 /**
  * @function Home
  * @return {XML}
  */
 function Home() {
-  const slider = [{
-    title: 'React.js',
-    subtitle: 'A JAVASCRIPT LIBRARY FOR BUILDING USER INTERFACES',
-    img: 'https://cdn.pixabay.com/photo/2017/01/12/16/12/banner-1975062_960_720.jpg',
-    imgAlt: 'React'
-  }, {
-    title: 'Redux.js',
-    subtitle: 'Redux is a predictable state container for JavaScript apps.',
-    img: 'https://cdn.pixabay.com/photo/2017/01/13/01/14/banner-1976086_960_720.jpg',
-    imgAlt: 'redux'
-  }, {
-    title: 'Material UI',
-    subtitle: 'Material-UI came about from our love of React and Google\'s Material Design.',
-    img: 'https://cdn.pixabay.com/photo/2017/01/13/01/22/banner-1976103_960_720.jpg',
-    imgAlt: 'redux'
-  },{
-    title: 'React Router',
-    subtitle: ' React Router is a collection of navigational components that compose declaratively with your application',
-    img: 'https://cdn.pixabay.com/photo/2017/01/12/16/15/banner-1975070_960_720.jpg',
-    imgAlt: 'redux'
-  }];
-  const sliderItems = slider.map((item, index) => <SliderItem key={index} {...item}/>);
-  return <Slider>{sliderItems}</Slider>;
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>Home Page</h1>
+      <p>A simple application for tracking your activity in a job quest </p>
+    </div>
+  );
 }
 
 export default Home;
