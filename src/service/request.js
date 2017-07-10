d2fe67969e81316e7d9fc175e3ff7602e3f202bc
peroperje <el.pionir@gmail.com>
2017-07-10 16:49:03 +0200

    heroku deploty

--- src/service/request.js
+++ src/service/request.js
@@ -5,46 +5,27 @@ import jwtStorage from '../store/jwtStorage';
  * Create an Axios Client with defaults
  */
 const client = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: process.env.NODE_ENV === 'production' ? 'api/' : 'http://localhost:3000/api/'
 });
-
-
-
 
 /**
  * @description Request Wrapper with default success/error actions
- * @param {Object} app call settings
+ * @param {Object} options app call settings
+ * @return {Promise}
  */
 const request = function (options) {
 
   const onSuccess = function (response) {
-    console.debug('Request Successful!', response);
     return response;
   };
 
   const onError = function (error) {
-    console.error('Request Failed:', error.config);
-
-    if (error.response) {
-      // Request was made but server responded with something
-      // other than 2xx
-      console.error('Full error:', error);
-      console.error('Status:', error.response.status);
-      console.error('Data:', error.response.data);
-      console.error('Headers:', error.response.headers);
-
-    } else {
-      // Something else happened while setting up the request
-      // triggered the error
-      console.error('Error Message:', error.message);
-    }
-
     return Promise.reject(error.response || error.message);
   };
 
   const jwt = jwtStorage.getJWT();
 
-  if(jwt){
+  if (jwt) {
     client.defaults.headers.common['x-auth'] = jwt;
   }
   return client(options)
