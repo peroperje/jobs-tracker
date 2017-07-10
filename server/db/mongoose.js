a09e3dc78b00ed396277c1cebaf8b20470c72333
peroperje <el.pionir@gmail.com>
2017-07-10 19:52:34 +0200

    add mLab env Variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 const mongoURL = process.env.MONGODB_URL || process.env.MONGODB_URI;
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(mongoURL);
 
 module.exports = {
   mongoose
